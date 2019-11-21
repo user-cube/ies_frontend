12a7d0a3caafcd24f0ffd9577da0fc79aa591aa2
Rui Coelho <ruicoelho@ua.pt>
2019-11-21 01:08:30 +0000

    Update 'force login'

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -28,22 +28,23 @@ export const login = credentials => dispatch =>
             }
         }
     );*/
-    {
-    localStorage.smartRoom_JWT = jwt.sign({"email":"ruicoelho@ua.pt"},"verysmart", {expiresIn:"1h"})
-    //localStorage.taca_uaJWT = user["token"];
-    try {
-        var decoded = jwt.verify(localStorage.getItem("smartRoom_JWT"), 'verysmart');
-        console.log(decoded)
-        const user = {
-            email: decoded.email
+    api.user.login(credentials).then(user =>
+        {
+            localStorage.smartRoom_JWT = jwt.sign({"email":"ruicoelho@ua.pt"},"verysmart", {expiresIn:"1h"})
+            //localStorage.taca_uaJWT = user["token"];
+            try {
+                var decoded = jwt.verify(localStorage.getItem("smartRoom_JWT"), 'verysmart');
+                console.log(decoded)
+                const user = {
+                    email: decoded.email
+                }
+                dispatch(userLoggedIn(user));
+            } catch(err) {
+                console.log(err)
+            }
         }
-        dispatch(userLoggedIn(user));
-    } catch(err) {
-        console.log(err)
-    }
-}
-export const logout = () => dispatch =>
-{
+    );
+export const logout = () => dispatch => {
     localStorage.removeItem("smartRoom_JWT");
     dispatch(userLoggedOut())
 };
--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 export default {
     user: {
-        login: (credentials) => axios.post('https://deti-engsoft-02:3000/login', credentials).then(res => res.data.user),
+        //login: (credentials) => axios.post('https://deti-engsoft-02:3000/login', credentials).then(res => res.data.user),
+        login: (credentials) => axios.get('https://eneeb.aneeb.pt/wp-json/wp/v2/users', credentials).then(res => res.data.id),
     }
 };
