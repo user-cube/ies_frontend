5ef5b47d0dcdd313bd052c0c72180a00f8735841
Rui Coelho <ruicoelho@ua.pt>
2019-11-25 12:08:20 +0000

    Fix login

--- src/components/Routes/NoAuth.jsx
+++ src/components/Routes/NoAuth.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import {Route} from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
 
-const NoAuth = ({component: Component, ...rest}) => (
+const NoAuth = ({ isAuthenticated, component: Component, ...rest }) => (
     <Route
         {...rest}
         render={props =>
-            <Component {...props} />
+            !isAuthenticated ?
+                <Component {...props} />
+                : (
+                    <Redirect to="/auth/home" />
+                )
         }
     />
 );
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.user.email
+    };
+}
 
-export default NoAuth;
+NoAuth.propTypes = {
+    component: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired
+};
+
+export default connect(mapStateToProps)(NoAuth);
