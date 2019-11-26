70fb85a7c3ced6cbfdb091198aa3610360b2c891
Rui Coelho <ruicoelho@ua.pt>
2019-11-26 00:24:55 +0000

    Consultas

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,22 +10,19 @@ import {
     Col,
 } from "reactstrap";
 
-// core components
-import {
-    chartExample3,
-} from "../variables/charts.jsx";
-
-
 import LastAcessos from "components/Tables/LastAccessTable.jsx";
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            co2_labels : [],
-            co2_values : [],
-            temp_labels : [],
-            temp_values : []
+            co2_labels: [],
+            co2_values: [],
+            temp_labels: [],
+            temp_values: [],
+            numero_acessos: [],
+            pessoas: [],
+            acessos : []
         };
     }
 
@@ -33,11 +30,11 @@ class Home extends React.Component {
 
         var token = localStorage.getItem("smartRoom_JWT");
 
-        axios.get('http://deti-engsoft-02.ua.pt:3000/co2/today', { headers: {"Authorization" : `Bearer ${token}`} })
+        axios.get('http://deti-engsoft-02.ua.pt:3000/co2/today', {headers: {"Authorization": `Bearer ${token}`}})
             .then(res => {
                 var co2 = [];
                 co2 = res.data;
-                if (co2.length === 0){
+                if (co2.length === 0) {
                     co2 = []
                 }
                 let lista_labels = [];
@@ -49,11 +46,11 @@ class Home extends React.Component {
                 this.setState({co2_labels: lista_labels, co2_values: lista_values});
             });
 
-        axios.get('http://deti-engsoft-02.ua.pt:3000/temperature/today', { headers: {"Authorization" : `Bearer ${token}`} })
+        axios.get('http://deti-engsoft-02.ua.pt:3000/temperature/today', {headers: {"Authorization": `Bearer ${token}`}})
             .then(res => {
                 var temp = [];
                 temp = res.data;
-                if (temp.length === 0){
+                if (temp.length === 0) {
                     temp = []
                 }
                 let lista_labels = [];
@@ -63,6 +60,41 @@ class Home extends React.Component {
                     lista_values.push(valor["temp"])
                 })
                 this.setState({temp_labels: lista_labels, temp_values: lista_values});
+            });
+
+        axios.get('http://localhost:3000/access/today', {headers: {"Authorization": `Bearer ${token}`}})
+            .then(res => {
+                var access = [];
+                access = res.data;
+                if (access.length === 0) {
+                    access = []
+                }
+                let pessoas = [];
+                access.forEach(valor => {
+                    pessoas.push(valor["user"])
+                })
+                var lista_pessoas = []
+                var lista_acessos = []
+                var pessoa = "";
+                var counter = 1;
+                pessoas = pessoas.sort()
+                for (let i = 0; i <= pessoas.length; i++) {
+                    if (pessoa === "") {
+                        pessoa = pessoas[i];
+                        lista_pessoas.push(pessoas[i])
+                    }
+                    if (pessoa === pessoas[i]) {
+                        counter++;
+                    } else {
+                        if (i !== pessoas.length) {
+                            lista_pessoas.push(pessoas[i])
+                        }
+                        lista_acessos.push(counter);
+                        counter = 1;
+                        pessoa = pessoas[i];
+                    }
+                }
+                this.setState({pessoas: lista_pessoas, numero_acessos: lista_acessos, acessos: access});
             });
     }
 
@@ -83,7 +115,7 @@ class Home extends React.Component {
                                 </CardHeader>
                                 <CardBody>
                                     <div className="chart-area">
-                                        <LastAcessos jogosLast={this.state.jogosLast} logos={this.state.logos} />
+                                        <LastAcessos acessosX={this.state.acessos}/>
                                     </div>
                                 </CardBody>
                             </Card>
@@ -197,8 +229,74 @@ class Home extends React.Component {
                                 <CardBody>
                                     <div className="chart-area">
                                         <Bar
-                                            data={chartExample3.data}
-                                            options={chartExample3.options}
+                                            data={canvas => {
+                                                let ctx = canvas.getContext("2d");
+
+                                                let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+                                                gradientStroke.addColorStop(1, "rgba(72,72,176,0.1)");
+                                                gradientStroke.addColorStop(0.4, "rgba(72,72,176,0.0)");
+                                                gradientStroke.addColorStop(0, "rgba(119,52,169,0)"); //purple colors
+
+                                                return {
+                                                    labels: this.state.pessoas,
+                                                    datasets: [
+                                                        {
+                                                            label: "Nº de acessos",
+                                                            fill: true,
+                                                            backgroundColor: gradientStroke,
+                                                            hoverBackgroundColor: gradientStroke,
+                                                            borderColor: "#d048b6",
+                                                            borderWidth: 2,
+                                                            borderDash: [],
+                                                            borderDashOffset: 0.0,
+                                                            data: this.state.numero_acessos
+                                                        }
+                                                    ]
+                                                }
+                                            }}
+                                            options={{
+                                                maintainAspectRatio: false,
+                                                legend: {
+                                                    display: false
+                                                },
+                                                tooltips: {
+                                                    backgroundColor: "#f5f5f5",
+                                                    titleFontColor: "#333",
+                                                    bodyFontColor: "#666",
+                                                    bodySpacing: 4,
+                                                    xPadding: 12,
+                                                    mode: "nearest",
+                                                    intersect: 0,
+                                                    position: "nearest"
+                                                },
+                                                responsive: true,
+                                                scales: {
+                                                    yAxes: [
+                                                        {
+                                                            gridLines: {
+                                                                drawBorder: false,
+                                                                color: "rgba(225,78,202,0.1)",
+                                                                zeroLineColor: "transparent"
+                                                            },
+                                                            ticks: {
+                                                                suggestedMin: 0,
+                                                                padding: 20,
+                                                                fontColor: "#9e9e9e"
+                                                            }
+                                                        }
+                                                    ],
+                                                    xAxes: [
+                                                        {
+                                                            gridLines: {
+                                                                drawBorder: false,
+                                                                color: "rgba(225,78,202,0.1)",
+                                                                zeroLineColor: "transparent"
+                                                            },
+                                                        }
+                                                    ]
+                                                }
+                                            }}
                                         />
                                     </div>
                                 </CardBody>
@@ -225,68 +323,69 @@ class Home extends React.Component {
                                                 gradientStroke.addColorStop(0, "rgba(66,134,121,0)"); //green colors
 
                                                 return {
-                                                labels: this.state.co2_labels,
+                                                    labels: this.state.co2_labels,
                                                     datasets: [
-                                            {
-                                                label: "C02",
-                                                fill: true,
-                                                backgroundColor: gradientStroke,
-                                                borderColor: "#00d6b4",
-                                                borderWidth: 2,
-                                                borderDash: [],
-                                                borderDashOffset: 0.0,
-                                                pointBackgroundColor: "#00d6b4",
-                                                pointBorderColor: "rgba(255,255,255,0)",
-                                                pointHoverBackgroundColor: "#00d6b4",
-                                                pointBorderWidth: 20,
-                                                pointHoverRadius: 4,
-                                                pointHoverBorderWidth: 15,
-                                                pointRadius: 4,
-                                                data: this.state.co2_values
-                                            }
-                                                ]
-                                            };
+                                                        {
+                                                            label: "C02",
+                                                            fill: true,
+                                                            backgroundColor: gradientStroke,
+                                                            borderColor: "#00d6b4",
+                                                            borderWidth: 2,
+                                                            borderDash: [],
+                                                            borderDashOffset: 0.0,
+                                                            pointBackgroundColor: "#00d6b4",
+                                                            pointBorderColor: "rgba(255,255,255,0)",
+                                                            suggestedMax: 15,
+                                                            pointHoverBackgroundColor: "#00d6b4",
+                                                            pointBorderWidth: 20,
+                                                            pointHoverRadius: 4,
+                                                            pointHoverBorderWidth: 15,
+                                                            pointRadius: 4,
+                                                            data: this.state.co2_values
+                                                        }
+                                                    ]
+                                                };
                                             }}
                                             options={{
                                                 maintainAspectRatio: false,
                                                 legend: {
-                                                display: false
-                                            },
+                                                    display: false
+                                                },
 
                                                 tooltips: {
-                                                backgroundColor: "#f5f5f5",
-                                                titleFontColor: "#333",
-                                                bodyFontColor: "#666",
-                                                bodySpacing: 4,
-                                                xPadding: 12,
-                                                mode: "nearest",
-                                                intersect: 0,
-                                                position: "nearest"
-                                            },
+                                                    backgroundColor: "#f5f5f5",
+                                                    titleFontColor: "#333",
+                                                    bodyFontColor: "#666",
+                                                    bodySpacing: 4,
+                                                    xPadding: 12,
+                                                    mode: "nearest",
+                                                    intersect: 0,
+                                                    position: "nearest"
+                                                },
                                                 responsive: true,
                                                 scales: {
-                                                yAxes: [
-                                            {
-                                                barPercentage: 1.6,
-                                                gridLines: {
-                                                drawBorder: false,
-                                                color: "rgba(29,140,248,0.0)",
-                                                zeroLineColor: "transparent"
-                                            },
-                                            }
-                                                ],
+                                                    yAxes: [
+                                                        {
+                                                            barPercentage: 1.6,
+                                                            gridLines: {
+                                                                drawBorder: false,
+                                                                color: "rgba(29,140,248,0.0)",
+                                                                zeroLineColor: "transparent"
+                                                            },
+                                                        }
+                                                    ],
 
-                                                xAxes: [
-                                            {
-                                                barPercentage: 1.6,
-                                                gridLines: {
-                                                drawBorder: false,
-                                                color: "rgba(0,242,195,0.1)",
-                                                zeroLineColor: "transparent"
-                                            },
-                                            }
-                                                ]
-                                            }
+                                                    xAxes: [
+                                                        {
+                                                            barPercentage: 1.6,
+                                                            gridLines: {
+                                                                drawBorder: false,
+                                                                color: "rgba(0,242,195,0.1)",
+                                                                zeroLineColor: "transparent"
+                                                            },
+                                                        }
+                                                    ]
+                                                }
                                             }}
                                         />
                                     </div>
